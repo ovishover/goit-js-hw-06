8c0310f9879d9dd3f301d76d69d65c8c75171593
Nikita Hudz <nikita.gudz8@gmail.com>
2022-05-21 14:11:46 +0300

    complete task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const array = [];
+for (const item of ingredients) {
+  const liItem = document.createElement('li');
+  liItem.textContent = item;
+  liItem.classList.add('item');
+  array.push(liItem);
+}
+
+const ulList = document.querySelector('#ingredients');
+ulList.append(...array);
