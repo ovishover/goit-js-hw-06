7c93efbc3949412205ecabf0d1acb21d42799558
Nikita Hudz <nikita.gudz8@gmail.com>
2022-05-25 14:41:19 +0300

    add new decision

--- js/task-02.js
+++ js/task-02.js
@@ -1,18 +1,28 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
 const array = [];
 for (const item of ingredients) {
-  const liItem = document.createElement('li');
+  const liItem = document.createElement("li");
   liItem.textContent = item;
-  liItem.classList.add('item');
+  liItem.classList.add("item");
   array.push(liItem);
 }
 
-const ulList = document.querySelector('#ingredients');
+const ulList = document.querySelector("#ingredients");
 ulList.append(...array);
+
+// const array = [];
+// ingredients.forEach(function (ingredient) {
+//   const listItem = document.createElement("li");
+//   listItem.textContent = ingredient;
+//   listItem.classList.add("item");
+//   array.push(listItem);
+// });
+// const list = document.querySelector("#ingredients");
+// list.append(...array);
