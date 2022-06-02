917e0f2068b3ebc2a5407a62103b783a2e9a3e67
Nikita Hudz <nikita.gudz8@gmail.com>
2022-06-02 19:22:00 +0300

    fix task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,21 +1,23 @@
 const ingredients = [
-  "Potatoes",
-  "Mushrooms",
-  "Garlic",
-  "Tomatos",
-  "Herbs",
-  "Condiments",
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
 ];
-const array = [];
-for (const item of ingredients) {
-  const liItem = document.createElement("li");
-  liItem.textContent = item;
-  liItem.classList.add("item");
-  array.push(liItem);
-}
 
-const ulList = document.querySelector("#ingredients");
-ulList.append(...array);
+const ulList = document.querySelector('#ingredients');
+
+// const array = [];
+// for (const item of ingredients) {
+//   const liItem = document.createElement('li');
+//   liItem.textContent = item;
+//   liItem.classList.add('item');
+//   array.push(liItem);
+// }
+
+// ulList.append(...array);
 
 // const array = [];
 // ingredients.forEach(function (ingredient) {
@@ -26,3 +28,11 @@ ulList.append(...array);
 // });
 // const list = document.querySelector("#ingredients");
 // list.append(...array);
+
+const result = ingredients.map(e => {
+  const ads = document.createElement('li');
+  ads.textContent = e;
+  ads.classList.add('item');
+  return ads;
+});
+ulList.append(...result);
