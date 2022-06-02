cedb9a1d8fe9cf06dbf0dd246fd0c57ba6498b06
Nikita Hudz <nikita.gudz8@gmail.com>
2022-06-02 19:25:33 +0300

    fix task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,14 @@
 const countCatagory = document.querySelectorAll('.item');
 console.log(`Number of categories: ${countCatagory.length}`);
 
-for (const item of countCatagory) {
+// for (const item of countCatagory) {
+//   console.log(`Categore: ${item.firstElementChild.textContent}`);
+//   const countItems = item.querySelectorAll('li');
+//   console.log(`Elements: ${countItems.length}`);
+// }
+
+countCatagory.forEach(item => {
   console.log(`Categore: ${item.firstElementChild.textContent}`);
   const countItems = item.querySelectorAll('li');
   console.log(`Elements: ${countItems.length}`);
-}
+});
