40b979e12c48dc266e34b9d265b80366e3cdce67
Nikita Hudz <nikita.gudz8@gmail.com>
2022-06-02 20:12:11 +0300

    fix task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,17 +1,3 @@
-const CounterPlugin = function ({ startValue = 0, stepCounter = 1 } = {}) {
-  this._value = startValue;
-  this._step = stepCounter;
-};
-
-CounterPlugin.prototype.decrement = function () {
-  this._value -= this._step;
-};
-
-CounterPlugin.prototype.increment = function () {
-  this._value += this._step;
-};
-// const myCounter = new CounterPlugin({ stepCounter: 100 });
-
 const incrBtn = document.querySelector("[data-action='increment']");
 const decrBtn = document.querySelector("[data-action='decrement']");
 const valueNum = document.querySelector('#value');
